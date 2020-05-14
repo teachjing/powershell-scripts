94e63a43f87d8746844a7ca945e736471f6a906e
Jing <jnghik@gmail.com>
2020-05-14 10:29:24 -0500

    Updates
    Added Parsing script

---
+++ Administrative/WorkInProgress/Parsing.ps1
@@ -0,0 +1,19 @@
+﻿$LogFilePath = "$env:windir\setupact.log"
+
+$LogContent = Get-Content -Path $LogFilePath |  ## This Gets the Content of the log file and pipes to for loop.
+    ForEach-Object {
+	    # Splits Content by each line
+        $line = $_
+
+        # split the text with ":" delimiter 
+        $infos = $line -split '\t'
+
+        # create ordered hashtable and add information into hash table 
+        $hashtable = [Ordered]@{}
+        $hashtable.Date = $infos[0]
+        $hashtable.Next = $infos[1]
+
+        New-Object -TypeName PSObject -Property $hashtable
+    }
+
+$LogContent | Out-GridView
--- Microsoft/O365/O365-SecurityAssessment.ps1
+++ Microsoft/O365/O365-SecurityAssessment.ps1
@@ -5,6 +5,7 @@
 #       
 #       Tool Requires Requires EXO-Connect Session - https://aka.ms/exops-docs which will be installed by script
 #
+Set-ExecutionPolicy RemoteSigned
 
 function O365-checkcredentials() {
     Param (
@@ -19,8 +20,8 @@ function O365-checkcredentials() {
         write-host 'No sessions looks to exist. Lets create one.'
         #$UserCredentials = Get-Credential
         #Get-PSSession | Remove-PSSession
-        $Session = Connect-ExchangeOnline -UserPrincipalName $upn -ShowProgress $true 
-        #$Session = New-PSSession -name 'O365-Session' -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredentials -Authentication Basic -AllowRedirection
+        #$Session = Connect-ExchangeOnline -UserPrincipalName $upn -ShowProgress $true 
+        $Session = New-PSSession -name 'ExchangeOnline' -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredentials -Authentication Basic -AllowRedirection
         
     } 
 }
@@ -45,6 +46,7 @@ $WinRMRunning = (Get-Service 'WinRM').Status
 if ($WinRMRunning -ne "Running") {
     $enablePSRemoting = Read-Host -Prompt "WinRM is not detected to be running, Press enter to install."
     Enable-PSRemoting -Force    ## https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/enable-psremoting?view=powershell-7
+    Winrm quickconfig
     winrm get winrm/config/client/auth
 } else {
     Write-Host "WinRM looks to be running..."
