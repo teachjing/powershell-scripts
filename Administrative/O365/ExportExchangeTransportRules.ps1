c913e5faa1c3d6dfff981f8aba7000903683058a
jingsta <jnghik@gmail.com>
2021-09-22 07:28:16 -0500

    Create ExportExchangeTransportRules.ps1
    Added script to export and import transport rules

---
+++ Administrative/O365/ExportExchangeTransportRules.ps1
@@ -0,0 +1,14 @@
+﻿## Connect to exchange online
+
+Import-Module ExchangeOnlineManagement
+
+Connect-ExchangeOnline -UserPrincipalName "blah@domain.com"
+
+## Exports Exchange transport rules to C:\temp folder ##
+$filePath = "C:\temp\ExchangeTransportRules.xml"
+$file = Export-TransportRuleCollection
+Set-Content -Path $filePath -Value $file.FileData -Encoding Byte
+
+## Import Exchange Transport Rules
+[Byte[]]$Data = Get-Content -Path $filePath -Encoding Byte -ReadCount 0
+Import-TransportRuleCollection -FileData $Data
